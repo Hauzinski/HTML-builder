79a834d2abd1bed086d74b3c08f982e96f662507
Vitali Hauzinski <hauzinski@gmail.com>
2021-11-05 13:57:19 +0300

    fix: change double quotes with single quotes

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,9 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const txtPath = path.join(__dirname, "text.txt");
-const readableStream = fs.createReadStream(txtPath, "utf8");
+const txtPath = path.join(__dirname, 'text.txt');
+const readableStream = fs.createReadStream(txtPath, 'utf8');
 
-readableStream.on("data", function (chunk) {
+readableStream.on('data', function (chunk) {
   console.log(chunk);
 });
--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,38 +1,38 @@
-const fs = require("fs");
-const path = require("path");
-const readline = require("readline");
-const process = require("process");
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const process = require('process');
 
-const txtPath = path.join(__dirname, "text.txt");
+const txtPath = path.join(__dirname, 'text.txt');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 let writeableStream;
 
-console.log("Hi. You can write your text here. Press 'ENTER' to save the message to a file and move to a new line.");
-console.log("To exit the program, enter the word 'exit' or press the key combination Ctrl + C.");
-console.log("Write your message:");
-rl.on("line", (message) => {
-  if (!writeableStream && message !== "exit") {
+console.log('Hi. You can write your text here. Press "ENTER" to save the message to a file and move to a new line.');
+console.log('To exit the program, enter the word "exit" or press the key combination Ctrl + C.');
+console.log('Write your message:');
+rl.on('line', (message) => {
+  if (!writeableStream && message !== 'exit') {
     writeableStream = fs.createWriteStream(txtPath);
   }
   writeMessage(message);
 });
 
 const goodBuy = () => {
-  console.log("Goodbuy! See you later!");
+  console.log('Goodbuy! See you later!');
 };
 
 const writeMessage = (message) => {
-  if (message === "exit") { 
-    goodBuy();   
+  if (message === 'exit') {
+    goodBuy();
     process.exit();
   }
   writeableStream.write(`${message} \n`);
-  fs.createReadStream(txtPath, "utf8");
+  fs.createReadStream(txtPath, 'utf8');
 };
 
-process.on("beforeExit", () => {
+process.on('beforeExit', () => {
   goodBuy();
 });
