77e9a238faa29ef0550f34b7ef7987cb46988d1c
Vitali Hauzinski <hauzinski@gmail.com>
2021-11-08 13:17:30 +0300

    feat: add task '04 copy directory'

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const { mkdir, copyFile, readdir} = require('fs').promises;
+const path = require('path');
 
+async function getFiles() {
+  try {
+    const dirPath = path.join(__dirname, 'files');
+    const newDirPath = path.join(__dirname, 'files-copy');    
+    const files = await readdir(dirPath, { withFileTypes: true });    
+    for (const file of files) {
+      if (file.isFile()) {
+        const filePath = path.join(dirPath, file.name);        
+        const newFilePath = path.join(newDirPath, file.name);        
+        await copyFile(filePath, newFilePath);
+      }
+    }
+    console.log('Files copied to new directory');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function makeDir() {
+  try {
+    await mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+  } catch (err) {
+    console.error(err);
+  }
+  console.log('New directory "files-copy" created');
+}
+
+async function copyDir() {
+  await makeDir();
+  await getFiles();
+}
+copyDir();
