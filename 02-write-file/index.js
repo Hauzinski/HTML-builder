e73e7052343a385fde8293e1c53ecb786980a617
Vitali Hauzinski <hauzinski@gmail.com>
2021-11-08 13:34:15 +0300

    fix: fix code task '02 write file' according to the check

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -8,15 +8,12 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-let writeableStream;
+let writeableStream = fs.createWriteStream(txtPath);
 
 console.log('Hi. You can write your text here. Press "ENTER" to save the message to a file and move to a new line.');
 console.log('To exit the program, enter the word "exit" or press the key combination Ctrl + C.');
 console.log('Write your message:');
 rl.on('line', (message) => {
-  if (!writeableStream && message !== 'exit') {
-    writeableStream = fs.createWriteStream(txtPath);
-  }
   writeMessage(message);
 });
 
@@ -29,8 +26,7 @@ const writeMessage = (message) => {
     goodBuy();
     process.exit();
   }
-  writeableStream.write(`${message} \n`);
-  fs.createReadStream(txtPath, 'utf8');
+  writeableStream.write(`${message} \n`);  
 };
 
 process.on('beforeExit', () => {
